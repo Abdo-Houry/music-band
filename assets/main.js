32ffc3466c6f6055b078d718a69d541a98939553
abdulaziz-houry <abdalazizhoury@gmail.com>
2023-09-20 02:58:14 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+
+  
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 500) scrollTop.classList.add('show-scroll');
+  else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
